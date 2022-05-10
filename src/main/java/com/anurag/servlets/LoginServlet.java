df67c9034a2b1d3c2325a601b4ce8d07f77a27bf
Anurag Kulkarni <kanurag0801@gmail.com>
2022-05-10 11:10:43 +0530

    Added JSP page and redirected from servlet to jsp

--- src/main/java/com/anurag/servlets/LoginServlet.java
+++ src/main/java/com/anurag/servlets/LoginServlet.java
@@ -1,7 +1,6 @@
 package com.anurag.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,7 +13,6 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        out.print("Dummy Stuff!");
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
     }
 }
