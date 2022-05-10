6d18a88803f48c84971fcbf8dd1c44417635093f
Anurag Kulkarni <kanurag0801@gmail.com>
2022-05-10 13:06:17 +0530

    used .equals method

--- src/main/java/com/anurag/servlets/UserValidateService.java
+++ src/main/java/com/anurag/servlets/UserValidateService.java
@@ -2,7 +2,7 @@ package com.anurag.servlets;
 
 public class UserValidateService {
     public boolean validateUser(String username, String password){
-        if(username == "virat" && password == "virat123"){
+        if(username.equals("virat") && password.equals("virat123")){
             return true;
         }
         return false;
